5279d1272f530a9bbab9069c41be97c6e0d610c3
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-02-20 14:12:49 +0100

    ejercicio1

---
+++ sumar.py
@@ -0,0 +1,13 @@
+# rpograma que pida al usuario dos valores y un operador 
+
+valor1 = input("Introduce el primer digito: ")
+valor1=int(valor1)
+
+valor2 = input("Introduce el segundo: ")
+valor2=int(valor2)
+
+resultado = valor1 + valor2
+print (valor1,"+",valor2,"=",resultado)
+
+resultado = valor1 * valor2
+print (valor1,"*",valor2,"=",resultado)
